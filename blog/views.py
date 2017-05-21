7ec27f7e554764b354989927badfad8fc31f26de
Андрей <strilkts@gmail.com>
2017-05-21 23:06:08 +0300

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
 
 def post_list(request):
@@ -26,8 +26,6 @@ def post_new(request):
         else:
                 form = PostForm()
         return render(request, 'blog/post_edit.html', {'form': form})
-
-
 def post_edit(request, pk):
         post = get_object_or_404(Post, pk=pk)
         if request.method == "POST":
@@ -41,3 +39,15 @@ def post_edit(request, pk):
         else:
                 form = PostForm(instance=post)
         return render(request, 'blog/post_edit.html', {'form': form})
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
